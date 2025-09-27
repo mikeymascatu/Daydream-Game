0e383adb158f4e7df45d37d7c6cb22234dbf510d
Michael Panait <166886630+mikeymascatu@users.noreply.github.com>
2025-09-27 15:10:30 -0400

    fighting continuously

--- Assets/Scripts/PlayerControllerV2.cs
+++ Assets/Scripts/PlayerControllerV2.cs
@@ -7,7 +7,7 @@ public class PlayerControllerV2 : MonoBehaviour {
     public float jumpForce;
     private float moveInput;
 
-    private Rigidbody2D rb;                                                     
+    private Rigidbody2D rb;
 
     private bool facingRight = true;
 
@@ -20,42 +20,43 @@ public class PlayerControllerV2 : MonoBehaviour {
     private int extraJumps;
 
 
-    void start(){
+    void start() {
         extraJumps = extraJumpsValue;
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate(){
+    void FixedUpdate() {
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
 
         moveInput = Input.GetAxis("Horizontal");
         Debug.Log(moveInput);
-        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y); 
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
 
-        if(facingRight == false && moveInput > 0){
+        if (facingRight == false && moveInput > 0) {
             Flip();
-        } else if(facingRight == true && moveInput < 0) {
+        } else if (facingRight == true && moveInput < 0) {
             Flip();
-        }
-
-        void Update(){
-            if(isGrounded == true){
-                extraJumps = extraJumpsValue;
-            }
-            if(Input.GetKeyDown(KeyCode.Space) && extraJumps > 0 ){
-                rb.linearVelocity = Vector2.up * jumpForce;
-                extraJumps--;
-            } else if(Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded == true){
-                rb.linearVelocity = Vector2.up * jumpForce;
-            }
-        }
-
-        void Flip(){
-            facingRight = !facingRight;
-            Vector3 Scaler = transform.localScale;
-            Scaler.x *= -1;
-            transform.localScale = Scaler;
         }
     }
+
+    void Update() {
+        if (isGrounded == true) {
+            extraJumps = extraJumpsValue;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0) {
+            rb.linearVelocity = Vector2.up * jumpForce;
+            extraJumps--;
+        } else if (Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded == true) {
+            rb.linearVelocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void Flip(){
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+    
 }
