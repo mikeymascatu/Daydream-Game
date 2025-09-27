8d20f58c7f0769c050c818fc2c8bf73427b6f076
subtlemoon <jaewonleee333@gmail.com>
2025-09-27 17:13:53 -0400

    yes

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate()
+    {
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+    }
+}
