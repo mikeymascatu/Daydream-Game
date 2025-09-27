3a0db2bf0d2d46cf8014b901b99d131e5c2e5d6c
subtlemoon <jaewonleee333@gmail.com>
2025-09-27 11:55:27 -0400

    11:55:20 Camera Following

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector2 offset;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = target.position * offset;
+    }
+}
