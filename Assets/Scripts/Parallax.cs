c9e50059acc8bca7a6cb4955ac4e1cdca5453d70
subtlemoon <jaewonleee333@gmail.com>
2025-09-27 18:01:17 -0400

    final final parallax

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -13,12 +13,8 @@ public class Parallax : MonoBehaviour
 
     void FixedUpdate()
     {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
 
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
-
-        if (temp > startpos + length) startpos += length;
-        else if (temp < startpos - length) startpos -= length;
     }
 }
