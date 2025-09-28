4ac0039cdf7b744c25f0f80d2b6b78faa69c8720
subtlemoon <jaewonleee333@gmail.com>
2025-09-28 01:25:55 -0400

    animation (slash) and rain

---
+++ Assets/Scripts/Test animation.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class AnimatorTriggerTest : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    [SerializeField] string triggerName = "Q Attack"; // EXACT name from your Animator parameter
+    int trigHash;
+
+    void Awake()
+    {
+        if (!anim) anim = GetComponent<Animator>();
+        trigHash = Animator.StringToHash(triggerName);
+        Debug.Log($"[Test] Animator on: {anim?.gameObject.name}, controller: {anim?.runtimeAnimatorController}", anim);
+        if (anim && anim.layerCount > 0) anim.SetLayerWeight(0, 1f);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && anim)
+        {
+            anim.ResetTrigger(trigHash); // optional
+            anim.SetTrigger(trigHash);
+            Debug.Log("[Test] Fired trigger: " + triggerName);
+        }
+    }
+}
