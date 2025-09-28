1346769fb03461ac075a894d641fbec4a496cc80
Michael Panait <166886630+mikeymascatu@users.noreply.github.com>
2025-09-28 11:19:01 -0400

    inventory pt2

--- Assets/Scripts/WorldItem.cs
+++ Assets/Scripts/WorldItem.cs
@@ -1,6 +1,29 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
-public class WorldItem
+[RequireComponent(typeof(Collider2D))]
+public class WorldItem : MonoBehaviour
 {
-    
+    public Item item;
+    public int amount = 1;
+    public bool autoPickup = true; // set true for instant pickup on touch
+
+    void Reset()
+    {
+        GetComponent<Collider2D>().isTrigger = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (autoPickup)
+        {
+            var inv = other.GetComponent<PlayerInventory>();
+            if (inv != null)
+            {
+                inv.Pickup(item, amount);
+                Destroy(gameObject);
+            }
+        }
+    }
 }
