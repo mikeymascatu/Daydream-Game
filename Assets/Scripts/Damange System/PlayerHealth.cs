28b148ffedf04d9a0aaf1b53b9fc36339c081bd0
Michael Panait <166886630+mikeymascatu@users.noreply.github.com>
2025-09-28 14:10:26 -0400

    solved immortality

--- Assets/Scripts/Damange System/PlayerHealth.cs
+++ Assets/Scripts/Damange System/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour, IDamageable
 {
@@ -48,7 +49,6 @@ public class PlayerHealth : MonoBehaviour, IDamageable
     void Die()
     {
         OnDied?.Invoke();
-        // Example: disable controls / respawn later
-        // gameObject.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
