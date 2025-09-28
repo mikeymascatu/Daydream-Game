0ac5b85a406a46f73b004cc97b721beade3d2a68
Michael Panait <166886630+mikeymascatu@users.noreply.github.com>
2025-09-28 08:47:42 -0400

    menus and stuff

---
+++ Assets/Scripts/FROM OLD GAME/InventoryMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InventoryMenu : MonoBehaviour
+{
+    public static bool InventoryIsActive = false;
+
+    public GameObject InventoryMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (InventoryIsActive)
+            {
+                Hide();
+            }
+            else
+            {
+                Show();
+            }
+        }
+    }
+
+    public void Hide()
+    {
+        InventoryMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        InventoryIsActive = false;
+    }
+    void Show()
+    {
+        InventoryMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        InventoryIsActive = true;
+    }
+}
+
